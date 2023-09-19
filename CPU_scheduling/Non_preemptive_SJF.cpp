b01c916e0fb94ad161767bf4ef6561fac7bd0d89
domohai <domohai12@gmail.com>
2023-09-19 15:26:05 +0700

    Non-preemptive SJF

---
+++ CPU_scheduling/Non_preemptive_SJF.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void input(vector<vector<int>>& data, int& number_of_processes);
+void calculate_time(const vector<vector<int>>& data, const int& number_of_processes);
+
+int main() {
+    cout << "Assume all processes arrive at the same time." << endl;
+    vector<vector<int>> data;
+    int number_of_processes = 0;
+    input(data, number_of_processes);
+    calculate_time(data, number_of_processes);
+    return 0;
+}
+
+void input(vector<vector<int>>& data, int& number_of_processes) {
+    vector<int> process_time;
+    cout << "Enter the number of processes: ";
+    cin >> number_of_processes;
+    cout << "Enter the busrt time of each process: ";
+    for (int i = 0; i < number_of_processes; i++) {
+        int temp;
+        cin >> temp;
+        process_time.push_back(temp);
+    }
+    sort(process_time.begin(), process_time.end());
+    data.push_back(process_time);
+}
+
+void calculate_time(const vector<vector<int>>& data, const int& number_of_processes) {
+    int temp = 0;
+    int average = 0;
+    for (int i = 0; i < data.size(); i++) {
+        for (int j = 0; j < data[i].size(); j++) {
+            cout << "P" << j+1 << " waiting time: " << temp << endl;
+            average += temp;
+            temp += data[i][j];
+        }
+    }
+    cout << "The average wating time: " << (float)average / number_of_processes << endl;
+}
